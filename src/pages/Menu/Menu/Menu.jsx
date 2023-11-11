f42f2c7ae3914982fe40419eaaa851e061f6e699
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-11 18:39:10 +0600

    create menu category

--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,20 +1,40 @@
 import { Helmet } from "react-helmet-async";
 import Cover from "../../Shared/Cover/Cover";
 import menuImg from '../../../assets/menu/banner3.jpg'
+import dessertImg from '../../../assets/menu/dessert-bg.jpeg'
 import MenuCategory from "../MenuCategory/MenuCategory";
+import useMenu from "../../../hooks/useMenu";
+import SectionTitle from "../../../components/SectionTile/SectionTitle";
 
 const Menu = () => {
+  const [menu] = useMenu()
+  const desserts = menu.filter(item => item.category === 'dessert')
+  const soups = menu.filter(item => item.category === 'soup')
+  const salads = menu.filter(item => item.category === 'salad')
+  const pizzas = menu.filter(item => item.category === 'pizza')
+  const offered = menu.filter(item => item.category === 'offered')
+  
   return (
     <div>
       <Helmet>
         <title>Bistro Boss | Menu</title>
       </Helmet>
+      {/* main cover */}
       <Cover
         img={menuImg}
         title='our menu'
         subtitle='Would you like to try a dish?'
       ></Cover>
-      <MenuCategory></MenuCategory>
+      <SectionTitle subheading='Dont miss' heading='Todays Offer'></SectionTitle>
+      {/* offered menu items  */}
+      <MenuCategory items={offered}></MenuCategory>
+
+      {/* dessert menu items */}
+      <MenuCategory items={desserts} title='Desserts' coverImg={dessertImg}></MenuCategory>
+
+      {/* pizza menu items  */}
+
+
     </div>
   );
 };
--- src/pages/Menu/MenuCategory/MenuCategory.jsx
+++ src/pages/Menu/MenuCategory/MenuCategory.jsx
@@ -1,7 +1,20 @@
-const MenuCategory = () => {
+import Cover from "../../Shared/Cover/Cover";
+import MenuItem from "../../Shared/MenuItem/MenuItem";
+
+const MenuCategory = ({ items, coverImg, title }) => {
   return (
-    <div>
-      <p>Hello, I am MenuCategory</p>
+    <div className="pt-12">
+      {title && (
+        <Cover
+          img={coverImg}
+           title={title}
+        ></Cover>
+      )}
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-6 my-12">
+        {items?.map((item) => (
+          <MenuItem key={item._id} item={item}></MenuItem>
+        ))}
+      </div>
     </div>
   );
 };
