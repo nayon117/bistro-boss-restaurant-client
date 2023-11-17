a640bfdd1979e7b4436d5cd8578fa2a22833fd9f
nayon117 <hasibulhasannayon260@gmail.com>
2023-11-17 08:36:58 +0600

    secure admin route

---
+++ src/Route/AdminRoute.jsx
@@ -0,0 +1,18 @@
+import { Navigate, useLocation } from "react-router-dom";
+import useAdmin from "../hooks/useAdmin";
+import useAuth from "../hooks/useAuth";
+
+const AdminRoute = ({children}) => {
+    const [isAdmin, isAdminLoading] = useAdmin()
+    const {user,loading} = useAuth()
+    const location = useLocation()
+
+    if (loading || isAdminLoading) {
+        return <p>loading...</p>
+    }
+    if (user && isAdmin) {
+        return children;
+    }
+    return <Navigate state={{from:location}}  to='/login' replace></Navigate>
+};
+export default AdminRoute;
